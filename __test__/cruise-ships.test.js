dc3300d69dcc0b12de56d181f219c46cf10055c2
gergana <gergana_ruse@yahoo.com>
2018-03-10 09:03:36 +0000

    tests for cruise ships

--- __test__/cruise-ships.test.js
+++ __test__/cruise-ships.test.js
@@ -4,13 +4,14 @@ describe('CruiseShip', () => {
   it('it can be instantiated', () => {
     expect(new CruiseShip()).toBeInstanceOf(Object);
   });
+  it('it has a starting port', () => {
+    const ship = new CruiseShip('Portovenere');
+    expect(ship.startingPort).toBe('Portovenere');
+  });
+  it('can set sail', () => {
+    const ship = new CruiseShip();
+    ship.setSail();
+    expect(ship.startingPort).toBeFalsy();
+  });
 });
 
-/* //describe ('addPassengers', () => {
-   // it('adds the passengers to the ship', () =>{
-        const CruiseShip = new CruiseShip();
-        const mockCruiseShip = jest.fn();
-        CruiseShip.addPassengers(mockCruiseShip);
-        expect(CruiseShip.passengers).toContain([mockCruiseShip]) //an array goes here
-    });
-});*/
