8ca9b560711f224bfe9d1886d6c779de2b67a929
gergana <gergana_ruse@yahoo.com>
2018-03-11 15:01:46 +0000

    updated tests

--- __test__/cruise-ships.test.js
+++ __test__/cruise-ships.test.js
@@ -1,17 +1,27 @@
 const CruiseShip = require('../src/cruise-ships.js');
+const Port = require('../src/cruise-ships.js');
 
 describe('CruiseShip', () => {
   it('it can be instantiated', () => {
     expect(new CruiseShip()).toBeInstanceOf(Object);
   });
   it('it has a starting port', () => {
-    const ship = new CruiseShip('Portovenere');
-    expect(ship.startingPort).toBe('Portovenere');
+    const port = new Port('Dover');
+    const ship = new CruiseShip(port);
+    expect(ship.currentPort).toBe(port);
   });
   it('can set sail', () => {
-    const ship = new CruiseShip();
+    const port = new Port('Dover');
+    const ship = new CruiseShip(port);
     ship.setSail();
-    expect(ship.startingPort).toBeFalsy();
+    expect(ship.currentPort).toBeFalsy();
   });
+  it('docks at different port', () => {
+    const dover = new Port('Dover');
+    const ship = new CruiseShip(dover);
+    const calais = new Port('Calais');
+    ship.dock(calais);
+    expect(ship.currentPort).toBe(calais);
+  })
 });
 
