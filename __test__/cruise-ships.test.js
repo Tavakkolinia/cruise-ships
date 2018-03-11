c03c2363f03cbf7ac10f89103ecf1f2aaadaaefb
gergana <gergana_ruse@yahoo.com>
2018-03-11 20:26:40 +0000

    ship test

--- __test__/cruise-ships.test.js
+++ __test__/cruise-ships.test.js
@@ -1,27 +1,34 @@
 const CruiseShip = require('../src/cruise-ships.js');
-const Port = require('../src/cruise-ships.js');
+const Port = require('../src/Port.js');
+const Itinerary = require('../src/Itinerary.js');
 
 describe('CruiseShip', () => {
   it('it can be instantiated', () => {
-    expect(new CruiseShip()).toBeInstanceOf(Object);
+    const port = new Port('Dover');
+    const itinerary = new Itinerary([port]);
+    const ship = new CruiseShip(itinerary);
+    expect(ship).toBeInstanceOf(Object);
   });
   it('it has a starting port', () => {
     const port = new Port('Dover');
-    const ship = new CruiseShip(port);
+    const itinerary = new Itinerary([port]);
+    const ship = new CruiseShip(itinerary);
     expect(ship.currentPort).toBe(port);
   });
   it('can set sail', () => {
     const port = new Port('Dover');
-    const ship = new CruiseShip(port);
+    const itinerary = new Itinerary([port]);
+    const ship = new CruiseShip(itinerary);
     ship.setSail();
     expect(ship.currentPort).toBeFalsy();
   });
   it('docks at different port', () => {
     const dover = new Port('Dover');
-    const ship = new CruiseShip(dover);
     const calais = new Port('Calais');
-    ship.dock(calais);
+    const itinerary = new Itinerary([dover, calais]);
+    const ship = new CruiseShip(itinerary);
+    ship.dock();
     expect(ship.currentPort).toBe(calais);
-  })
+  });
 });
 
