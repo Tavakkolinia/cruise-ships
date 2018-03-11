7d871164c4fe923ec1b5b42f064f326e9f5382b9
gergana <gergana_ruse@yahoo.com>
2018-03-11 20:27:33 +0000

    updated version

--- src/cruise-ships.js
+++ src/cruise-ships.js
@@ -1,14 +1,17 @@
-function CruiseShip(currentPort) {
-  this.currentPort = currentPort;
+function CruiseShip(itinerary) {
+  this.itinerary = itinerary;
+  this.currentPort = itinerary.ports[0];
 }
-function Port(portName) {
-  this.portName = portName;
-}
+
+
 CruiseShip.prototype.setSail = function () {
   this.currentPort = false;
 };
-CruiseShip.prototype.dock = function (port) {
-  this.currentPort = port;
+CruiseShip.prototype.dock = function () {
+  const itinerary = this.itinerary;
+  const previousPortIndex = itinerary.ports.indexOf(this.currentPort);
+
+  this.currentPort = itinerary.ports[previousPortIndex + 1];
 };
 const ship = new CruiseShip();
 const port = new Port('Dover');
