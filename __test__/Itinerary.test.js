a54a45a23b47ffa413c595374d23711d8f466a61
gergana <gergana_ruse@yahoo.com>
2018-03-12 19:44:05 +0000

    Itinerary test

--- __test__/Itinerary.test.js
+++ __test__/Itinerary.test.js
@@ -1,6 +1,7 @@
 const Port = require('../src/Port.js');
 const Itinerary = require('../src/Itinerary.js');
 
+
 describe('Itinerary', () => {
   it('it can be instantiated', () => {
     expect(new Itinerary('Dover')).toBeInstanceOf(Object);
