2e9afab2286e652b3c667e0a6426eae7495e8321
gergana <gergana_ruse@yahoo.com>
2018-03-14 18:33:44 +0000

    port

--- src/Port.js
+++ src/Port.js
@@ -1,4 +1,11 @@
 function Port(portName) {
   this.portName = portName;
+  this.ships = [];
 }
+Port.prototype.addShip = function (ship) {
+  this.ships.push(ship);
+};
+Port.prototype.removeShip = function (ship, ships) {
+  this.ships.splice(ship, 1);
+};
 module.exports = Port;
