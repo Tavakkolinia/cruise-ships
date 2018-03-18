b6b44b764533173a999bc3b892a266b5c9866b58
gergana <gergana_ruse@yahoo.com>
2018-03-18 08:05:51 +0000

    ships test

--- __test__/cruise-ships.test.js
+++ __test__/cruise-ships.test.js
@@ -3,34 +3,28 @@ const Port = require('../src/Port.js');
 const Itinerary = require('../src/Itinerary.js');
 
 describe('CruiseShip', () => {
-  it('it can be instantiated', () => {
-    const port = new Port('Dover');
-    const itinerary = new Itinerary([port]);
-    const ship = new CruiseShip(itinerary);
-    expect(ship).toBeInstanceOf(Object);
-  });
-  it('it has a starting port', () => {
-    const port = new Port('Dover');
-    const itinerary = new Itinerary([port]);
-    const ship = new CruiseShip(itinerary);
-    expect(ship.currentPort).toBe(port);
-  });
-  it('can set sail', () => {
-    const port = new Port('Dover');
-    const itinerary = new Itinerary([port]);
-    const ship = new CruiseShip(itinerary);
-    ship.setSail();
-    expect(ship.currentPort).toBeFalsy();
-    
-  });
-  it('docks at different port', () => {
-    const dover = new Port('Dover');
-    const calais = new Port('Calais');
-    const itinerary = new Itinerary([dover, calais]);
-    const ship = new CruiseShip(itinerary);
-    ship.dock();
-    expect(ship.currentPort).toBe(calais);
-    expect(calais.ships).toContain(ship);
+  describe('with a port and itinerary', () => {
+    let ship;
+    let port;
+    beforeEach(() => {
+      port = new Port('Dover');
+      const itinerary = new Itinerary([port]);
+      ship = new CruiseShip(itinerary);
+    });
+    it('it can be instantiated', () => {
+      expect(ship).toBeInstanceOf(Object);
+    });
+    it('it has a starting port', () => {
+      expect(ship.currentPort).toBe(port);
+    });
+    it('can set sail', () => {
+      ship.setSail();
+      expect(ship.currentPort).toBeFalsy();
+      expect(port.ships).not.toContain(ship);
+    });
+    it('docks at different port', () => {
+      expect(port.ships).toContain(ship);
+    });
   });
   it('gets added to port of instantiation', () => {
     const port = new Port('Dover');
