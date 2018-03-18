69e23fa8bfaf007cb588950c990cbf9311084eef
gergana <gergana_ruse@yahoo.com>
2018-03-18 11:41:43 +0000

    itinerary test updated

--- __test__/Itinerary.test.js
+++ __test__/Itinerary.test.js
@@ -7,8 +7,8 @@ describe('Itinerary', () => {
     expect(new Itinerary('Dover')).toBeInstanceOf(Object);
   });
   it('can have ports', () => {
-    const dover = new Port('Dover');
-    const calais = new Port('Calais');
+    const dover = new Port(jest.fn());
+    const calais = new Port(jest.fn());
     const itinerary = new Itinerary([dover, calais]);
 
     expect(itinerary.ports).toEqual([dover, calais]);
