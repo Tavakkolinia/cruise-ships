a2ff1bd6ddfa486f5f0c0244c92a626207d76668
gergana <gergana_ruse@yahoo.com>
2018-04-02 09:34:38 +0100

    updated versions

--- src/Port.js
+++ src/Port.js
@@ -1,11 +1,19 @@
-function Port(portName) {
-  this.portName = portName;
-  this.ships = [];
-}
-Port.prototype.addShip = function (ship) {
-  this.ships.push(ship);
-};
-Port.prototype.removeShip = function (ship, ships) {
-  this.ships.splice(ship, 1);
-};
-module.exports = Port;
+/* globals window */
+(function exportPort() {
+  function Port(portName) {
+    this.portName = portName;
+    this.ships = [];
+  }
+  Port.prototype.addShip = function (ship) {
+    this.ships.push(ship);
+  };
+  Port.prototype.removeShip = function (ship, ships) {
+    this.ships.splice(ship, 1);
+  };
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = Port;
+  } else {
+    window.Port = Port;
+  }
+}());
+
--- src/cruise-ships.js
+++ src/cruise-ships.js
@@ -1,21 +1,27 @@
-function CruiseShip(itinerary) {
-  this.itinerary = itinerary;
-  this.currentPort = itinerary.ports[0];
-  this.currentPort.addShip(this);
+/* globals window */
+(function exportCruiseShip() {
+  function CruiseShip(itinerary) {
+    this.itinerary = itinerary;
+    this.currentPort = itinerary.ports[0];
+    this.currentPort.addShip(this);
+  }
 
-}
+  CruiseShip.prototype.setSail = function () {
+    this.currentPort.removeShip(this);
+    this.currentPort = false;
+    this.renderMessageBox(`Now departing ${ship.currentPort.name}`);
+  };
+  CruiseShip.prototype.dock = function () {
+    const itinerary = this.itinerary;
+    const previousPortIndex = itinerary.ports.indexOf(this.currentPort);
 
-CruiseShip.prototype.setSail = function () {
-  this.currentPort.removeShip(this);
-  this.currentPort = false;
-};
-CruiseShip.prototype.dock = function () {
-  const itinerary = this.itinerary;
-  const previousPortIndex = itinerary.ports.indexOf(this.currentPort);
-
-  this.currentPort = itinerary.ports[previousPortIndex + 1];
-  this.currentPort.addShip(this);
-};
-
-module.exports = CruiseShip;
+    this.currentPort = itinerary.ports[previousPortIndex + 1];
+    this.currentPort.addShip(this);
+  };
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = CruiseShip;
+  } else {
+    window.CruiseShip = CruiseShip;
+  }
+}());
 
